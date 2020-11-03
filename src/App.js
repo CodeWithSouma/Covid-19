b5d7bad5ae5484c61bf4de0ae0594bd83a275cee
Souma <souma.hitech@gmail.com>
2020-11-03 11:51:41 +0530

    add cards chart and countrypicker component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {Cards,Chart,CountryPicker} from './components';
+import style from "./App.module.css"
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>App</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={style.container}>
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CountryPicker(props) {
+    return (
+        <div>
+            <h1>CountryPicker</h1>
+        </div>
+    );
+}
+
+export default CountryPicker;
